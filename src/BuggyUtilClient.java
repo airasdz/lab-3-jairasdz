3547cb3dd52f2c162cda669b081f2fb40816231e
Aira San Diego <jsandieg@purdue.edu>
2018-10-04 17:23:35 -0400

    Add manual test of StringUtils in main

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,10 +3,10 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
-        System.out.println("In: first = " + "\"Sean\"" +
-                ", last = " + "\"Flannery\"");
+        System.out.println("In: first = " + "\"Aira\"" +
+                ", last = " + "\"San Diego\"");
         System.out.println("Out: " +
-        StringUtils.makePurdueUsername("Sean", "Flannery"));
+        StringUtils.makePurdueUsername("Aira", "San Diego"));
     }
 }
 
